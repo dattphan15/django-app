327b0e892e664ffcd896247c34d450b0e05a4834
Kevin Phan <ken.phan@live.ca>
2021-09-10 17:44:10 -0700

    configure allowed hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wfw9wd+ldw%lg$qis7be=)+38^(z+gx56ebqia1#1tlm(0s1x3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
